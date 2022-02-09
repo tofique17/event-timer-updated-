aa788031968161be915b37ec775076cb258a584b
Tofiq Dadasli <tofiq.dadash@gmail.com>
2022-02-10 00:13:46 +0400

    events pushing an empty array

--- script.js
+++ script.js
@@ -5,141 +5,66 @@ const descValue = document.querySelector("#description");
 const dateValue = document.querySelector("#event_date");
 const timeValue = document.querySelector("#event_time");
 
+const renderingSide = document.querySelector(".render_events")
+const eventData = [];
+const calendar = ["Yanvar" , "Fevral" , "Mart" , "Aprel" , "May" , "Iyun" , "Iyul", "Avqust" , "Sentyabr" , "Oktyabr" , "Noyabr" , "Dekabr" ];
+
 
 addBtn.addEventListener("click" , event => {
 
-    /* getting values from date input*/
-const eventItem =  {
+  event.preventDefault();
+
+   const eventItem = {
     title: titleInput.value,
     description: descValue.value,
     date: `${dateValue.value},${timeValue.value}`
 }
 
-let myDate = new Date(eventItem.date);
-let eventMonth = myDate.getMonth(myDate) + 1;
-let eventDay = myDate.getDate(myDate);
+//pushing events an empty array
+eventData.push(eventItem);
+renderEvents();
 
-
-/* switching number months to string ones*/
-    switch(eventMonth)
-  {
-    case 1:
-      eventMonth="Yanvar";
-      break;
-    case 2:
-        eventMonth="Fevral";
-      break;
-    case 3:
-        eventMonth="Mart";
-      break;
-    case 4:
-        eventMonth="Aprel";
-      break;
-    case 5:
-        eventMonth="May";
-      break;
-    case 6:
-        eventMonth="Iyun";
-      break;
-    case 7:
-        eventMonth="Iyul";
-      break;
-    case 8:
-        eventMonth="Avqust";
-      break;
-    case 9:
-        eventMonth="Sentyabr";
-      break;
-    case 10:
-        eventMonth="Oktyabr";
-      break;
-    case 11:
-        eventMonth="Noyabr";
-      break;
-    case 12:
-        eventMonth="Dekabr";
-      break;
-    default:
-        eventMonth="Invalid month";
-  }
-
-
-
-/* Showing event date and event title on the left side*/
-  let renderedEventsContainer = document.querySelector('.render_events');
-  let renderedEvent = document.createElement('div');
-  renderedEvent.classList.add("rendered_event_container");
-
-  let dateSide = document.createElement('div');
-  dateSide.classList.add("date_side");
-  dateSide.textContent = `${eventDay} ${eventMonth}`;
-
-  let titleSide = document.createElement('div');
-  titleSide.classList.add("title_side");
-  titleSide.textContent = titleInput.value;
-
-
-  renderedEvent.appendChild(dateSide);
-  renderedEvent.appendChild(titleSide);
-  renderedEventsContainer.appendChild(renderedEvent);
-
-  
-
-    /* using localStorage */
-    const dateStorage = `${eventDay} ${eventMonth}`
-    const eventStorage  = titleInput.value;
-   
-  if(dateStorage && eventStorage){
-    localStorage.setItem(dateStorage , eventStorage);
-    
-  }
-  
-  for(let i = 0 ; i < localStorage ; i++){
-    const dateStorage = localStorage.dateStorage(i);
-    const eventStorage = localStorage.getItem(dateStorage);
-}
-
-
-
-  /* dragging process */
-  renderedEvent.draggable = true;
-  const trashIcon = document.querySelector(".trash");
-  
-  renderedEvent.addEventListener('dragstart' , dragStart);
-  renderedEvent.addEventListener('dragend' , dragEnd);
-
-  trashIcon.addEventListener('dragover' , dragOver);
-  trashIcon.addEventListener('dragenter' , dragEnter);
-  trashIcon.addEventListener('dragdrop' , dragDrop);
-
-  //drag functions
-  function dragStart(){
-    setTimeout(() => this.className = "invisible" , 0);
-  }
-
-  function dragEnd(){
-    renderedEvent.className = 'rendered_event_container';
-    dateSide.className = 'date_side';
-    titleSide.className = 'title_side';
-  }
-
-  function dragOver(e){
-    e.preventDefault();
-    console.log("drag over");
-  }
-
-  function dragEnter(e){
-    e.preventDefault();
-    console.log("drag enter");
-  }
-
-  function dragDrop(){
-      trashIcon.append(renderedEvent);
-  }
-
-  event.preventDefault();
-  
 })
+
+
+
+const renderEvents = () => {
+
+  renderingSide.textContent = '';
+  eventData.map((item) => {
+
+
+//getting month and day values
+  let eventDate = new Date(item.date);
+  let eventMonth = eventDate.getMonth(eventDate) + 1;
+  let eventDay = eventDate.getDate(eventDate);
+
+  //switching number months to string version
+  let selectedMonth = calendar[eventMonth - 1];
+
+//creating event - event time combination
+    let renderedEventsContainer = document.querySelector('.render_events');
+    let renderedEvent = document.createElement('div');
+    renderedEvent.classList.add("rendered_event_container");
+  
+    let dateSide = document.createElement('div');
+    dateSide.classList.add("date_side");
+    dateSide.textContent = `${eventDay} 
+    ${selectedMonth}`;
+    
+    
+  let titleSide = document.createElement('div');
+    titleSide.classList.add("title_side");
+    titleSide.textContent = item.title;
+  
+  
+    renderedEvent.appendChild(dateSide);
+    renderedEvent.appendChild(titleSide);
+    renderedEventsContainer.appendChild(renderedEvent);
+   
+  })
+
+}
 
 
 
